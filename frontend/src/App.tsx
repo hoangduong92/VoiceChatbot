a62ebe009a7db6a824a6cfdced205e8195aff3f6
duongbibo <damdandau1512@gmail.com>
2024-09-22 12:07:19 +0900

    init project and cleaned to start

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className="App">
+      <div className="text-3xl font-bold">Hello duongbibo</div>
+    </div>
+  )
+}
+
+export default App
